a523d5fc449a1293ad7a7ec5acdbec8510d8c0bf
Farrell Raafi <farrell@odooabc.com>
2020-07-09 13:06:02 +0700

    Latest update

--- App.js
+++ App.js
@@ -1,26 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { WebView } from 'react-native-webview';
-//import { StyleSheet, Text, View } from 'react-native';
+import { ToastAndroid } from 'react-native';
+import { WebView } from 'react-native-webview'
+import * as FileSystem from 'expo-file-system';
+import * as MediaLibrary from 'expo-media-library';
+
+const js = `
+var original_html_click = HTMLElement.prototype.click;
+HTMLElement.prototype.click = function () {
+  var element = this;
+  if (this.href && this.href.slice(0, 5) === 'blob:') {
+    fetch(this.href).then(function (response) {return response.blob()}).then(function (blob) {
+      var reader = new FileReader();
+      reader.onloadend = function() {
+        window.ReactNativeWebView.postMessage(JSON.stringify({'name': element.download, 'data': reader.result}));
+      }
+      reader.readAsDataURL(blob);
+    });
+  }
+  else return original_html_click.bind(this)();
+}
+`;
+
+const download = async (file) => !(await FileSystem.writeAsStringAsync(FileSystem.cacheDirectory + file.name, file.data.split(';base64,')[1], {encoding: FileSystem.EncodingType.Base64})) && (await MediaLibrary.requestPermissionsAsync()) && !(await MediaLibrary.saveToLibraryAsync(FileSystem.cacheDirectory + file.name,)) && ToastAndroid.show('File Downloaded in DCIM', ToastAndroid.LONG); //{mimeType: file.data.split(';base64,')[0].split('data:')[1]});
 
 export default function App() {
   return (
-    <WebView allowFileAccess allowsInlineMediaPlayback source={{uri: 'https://web.whatsapp.com'}} userAgent={'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.80 Safari/537.36'}/>
+    <WebView allowFileAccess allowsInlineMediaPlayback onMessage={(event) => download(JSON.parse(event.nativeEvent.data))} injectedJavaScript={js} source={{uri: 'https://web.whatsapp.com'}} userAgent={'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.80 Safari/537.36'}/>
   )
 }
-  /*return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});*/
